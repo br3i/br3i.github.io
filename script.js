51cea3dd7dfa513c6242f24a28cc48f0ed26f5ca
br3i <b.alexis98@hotmail.es>
2024-04-08 00:00:36 -0500

    second commit

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ onload = () => {
   document.body.classList.remove("container");
 };
 
+document.addEventListener("DOMContentLoaded", function() {
+    var toggleBubblesBtn = document.getElementById("toggleBubbles");
+
+    toggleBubblesBtn.addEventListener("click", function() {
+        var bubblesContainer = document.querySelector("body > .bubbles");
+      if (bubblesContainer) {
+          if (bubblesContainer.hidden) {
+              bubblesContainer.hidden = false; // Mostrar si está oculto
+          } else {
+              bubblesContainer.hidden = true; // Ocultar si está visible
+          }
+      }
+    });
+});
+
